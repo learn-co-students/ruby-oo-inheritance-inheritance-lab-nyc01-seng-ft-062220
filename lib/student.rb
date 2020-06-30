89d36d3b0edad8702f7adb55618edcda80f8b243
vakas-786 <vakas0959@gmail.com>
2020-06-30 17:44:03 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,13 @@
 class Student < User 
 
     attr_accessor :knowledge
+    
     def initialize
         @knowledge = []    
     end
 
-    def learn(knowledge_string)
-        self.knowledge = knowledge_string
+    def learn(knowledge_string_new)
+        self.knowledge = knowledge_string_new
     end
-
 
 end
